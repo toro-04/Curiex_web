a656508f425bbba7ecc16eb29f8279bdfd2ebd94
Anmolpreet Singh <125199528+toro-04@users.noreply.github.com>
2025-04-23 18:21:45 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import HomePage from './components/homePage'
+import NavBar from './components/navBar'
+
+function App() {
+  return (
+    <>
+    <div className="noOverFlowH black" >
+      <NavBar/>
+      <HomePage/>
+    </div>
+    </>
+  )
+}
+
+export default App
