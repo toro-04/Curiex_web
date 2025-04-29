4d109758a8f4a85f791628cc0c9f0b9a2824778c
DEADPOOLpv <prathitvisenthemaker@gmail.com>
2025-04-29 12:03:11 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import About from './components/about'
 import HomePage from './components/homePage'
 import NavBar from './components/navBar'
-
 import EventsPage from './components/Eventspage'
 import ContactPage from './components/contactus'
 
